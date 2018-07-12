19bc204d55389213b6dfc6704e2d6c09098d6b4a
Daniel Parks <dp+git@oxidized.org>
2018-07-11 20:57:48 -0700

    Manage project based on LDAP group

--- p9admin/cli/project.py
+++ p9admin/cli/project.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import click
+import os
 import p9admin
+import sys
 
 @click.group()
 def project():
@@ -26,3 +28,45 @@ def show(name):
 def delete(name):
     """Delete a project and the objects within"""
     p9admin.project.delete_project(p9admin.OpenStackClient(), name)
+
+@project.command("ensure-ldap")
+@click.argument("name")
+@click.option("--group-cn", metavar="CN",
+    help="The name of the group in LDAP. Defaults to NAME.")
+@click.option("--uid", "-u", envvar='puppetpass_username')
+@click.option("--password", "-p",
+    prompt=not os.environ.has_key('puppetpass_password'),
+    hide_input=True,
+    default=os.environ.get('puppetpass_password', None))
+def ensure_ldap(name, group_cn, uid, password):
+    """Ensure a project exists based on an LDAP group"""
+
+    if not uid:
+        sys.exit("You must specify --uid USER to connect to LDAP")
+
+    if group_cn is None:
+        group_cn = name
+
+    client = p9admin.OpenStackClient()
+    client.logger.info("Ensuring all users exist and have their own projects")
+
+    ### FIXME this is common code with user.ensure_users
+    p9Users = p9admin.user.get_ldap_group_users(group_cn, uid, password)
+    if not p9Users:
+        sys.exit("LDAP group {} doesn't contain any users".format(group_cn))
+
+    for p9User in p9Users:
+        project = p9admin.project.ensure_project(client, p9User.name)
+        client.ensure_user(p9User, default_project=project)
+        client.grant_project_access(project, user=p9User.user)
+
+    client.logger.info("Ensuring group exists and has the correct members")
+    group = client.ensure_group(name)
+    users = [p9User.user for p9User in p9Users]
+    client.ensure_group_members(group, users, keep_others=False)
+
+    client.logger.info("Ensuring actual project exists")
+    project = p9admin.project.ensure_project(client, name)
+    client.grant_project_access(project, group=group)
+
+    print('Project "{}" [{}]'.format(project.name, project.id))

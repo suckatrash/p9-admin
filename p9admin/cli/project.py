769ef67a4489e9910966e92edca09027838fa1ed
Daniel Parks <dp+git@oxidized.org>
2018-07-11 19:55:55 -0700

    Simplify definition of project command group

--- p9admin/cli/project.py
+++ p9admin/cli/project.py
@@ -7,22 +7,22 @@ def project():
     """Manage projects"""
     pass
 
-@project.command("ensure")
+@project.command()
 @click.argument("name")
-def project_ensure(name):
+def ensure(name):
     """Ensure a project exists"""
     client = p9admin.OpenStackClient()
     project = p9admin.project.ensure_project(client, name)
     print('Project "{}" [{}]'.format(project.name, project.id))
 
-@project.command("show")
+@project.command()
 @click.argument("name")
-def project_show(name):
+def show(name):
     """Show a project and the objects within"""
     p9admin.project.show_project(p9admin.OpenStackClient(), name)
 
-@project.command("delete")
+@project.command()
 @click.argument("name")
-def project_delete(name):
+def delete(name):
     """Delete a project and the objects within"""
     p9admin.project.delete_project(p9admin.OpenStackClient(), name)
